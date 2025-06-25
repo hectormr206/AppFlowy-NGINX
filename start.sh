872ff46d151f2af89be19aa82a0566b5d35fd3eb
hectormr206 <hectormartinezresendiz@gmail.com>
2025-06-25 12:06:03 -0600

    Add init config

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
     # This script substitutes environment variables in the nginx template
     # and then starts the nginx server.
     
-    envsubst '${APPFLOWY_BACKEND_HOST},${APPFLOWY_BACKEND_PORT},${GOTRUE_BACKEND_HOST},${GOTRUE_BACKEND_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+    envsubst '${APPFLOWY_BACKEND_HOST},${APPFLOWY_BACKEND_PORT},${GOTRUE_BACKEND_HOST},${GOTRUE_BACKEND_PORT},${MINIO_PRIVATE_HOST},${MINIO_PRIVATE_PORT},${MINIO_API_HOST},${MINIO_API_PORT},${CORS_ORIGIN}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
     
     echo "Starting Nginx..."
     nginx -g 'daemon off;'
